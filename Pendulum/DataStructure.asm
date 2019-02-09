e9f50e66d4acddf0d74a6b40c73baa195765aa2d
Park Mion-ger <33119492+Mionger@users.noreply.github.com>
2019-02-09 15:14:17 +0800

    Create DataStructure.asm

---
+++ Pendulum/DataStructure.asm
@@ -0,0 +1,29 @@
+# Info
+#
+# Lv:(8bits)
+#     DigiEgg   00000000  0
+#     Baby I    00000001  1
+#     Baby II   00000010  2
+#     Rookie    00000011  3
+#     Champion  00000100  4
+#     Ultimate  00000101  5
+#     Mega      00000110  6
+# Type:(2bit)
+#     No 00   0
+#     Va 01   1
+#     Vi 10   2
+#     Da 11   3
+# Age:(6bits)
+# Hungry:(4bits)
+# Strength:(4bits)
+# No:(8bits)
+# DP:(4bits)
+# Train:(4bits)
+# Win:(8bits)
+# Battle:(8bits)
+#
+# total 56bits
+#
+# pixel
+# 16*16*4 
+# total 1024bits
